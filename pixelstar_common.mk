e6e5ed2d8a120132ab846d02039d66b0e9e1f2a0
Libra420T <ryantf420@gmail.com>
2024-10-20 10:11:28 -0400

    Zuma: Add back 64 Bit Support Gms Makefile & Flag

--- pixelstar_common.mk
+++ pixelstar_common.mk
@@ -17,6 +17,17 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.vendor.camera.extensions.package=com.google.android.apps.camera.services \
     ro.vendor.camera.extensions.service=com.google.android.apps.camera.services.extensions.service.PixelExtensions
 
+# EUICC
+ifneq ($(BOARD_WITHOUT_RADIO),true)
+# product permissions XML from stock
+PRODUCT_COPY_FILES += \
+    device/google/zuma/product-permissions-stock.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/product-permissions-stock.xml
+endif
+
+# GMS
+GMS_MAKEFILE=gms_64bit_only.mk
+TARGET_SUPPORTS_64_BIT_APPS := true
+
 # Google Assistant
 PRODUCT_PRODUCT_PROPERTIES += ro.opa.eligible_device=true
 
